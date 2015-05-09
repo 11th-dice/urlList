70cffdfb380469fe4d6b62ea3d0539693b774f1c
sisisin <azsisisin@gmail.com>
2015-05-09 10:51:51 -0400

    view list from DB datas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var Client = require('mariasql');
+var inspect = require('util').inspect;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var c = new Client();
+  c.connect({
+    host: '192.168.1.15',
+    user: 'appUser',
+    password: 'kszk3081abk',
+  });
+  
+  if (false && !req.param('url') && !req.param('title')) {
+    c.query('INSERT INTO urlList.urlListTitles (url,title)\n' +
+      'VALUES ('+req.param('url')+ ','+req.param('title')+');');
+  }
+  
+  var sql = 'use urlList;\n';
+  c.query(sql);
+  
+  sql = 'select * from urlListTitles inner join urlListStatus on urlListTitles.url = urlListStatus.url;';
+  c.query(sql)
+    .on('result',function (queryRes) {
+      queryRes.on('row',function (row) {
+        console.log('Result row: ' + inspect(row));
+        var obj = {};
+        obj.title = 'Express';
+      
+        obj.list = [{
+          title:'google',
+          url:'http://google.co.jp',
+          sts:0
+        },
+        {
+          title:'yahoo',
+          url:'http://yahoo.co.jp',
+          sts:1
+        },
+        {
+          title:row.title,
+          url:row.url,
+          sts:row.sts
+        }
+        ];
+        
+        res.render('index',obj);              
+      })
+      .on('end',function (info) {
+
+      });
+    });
+  
+  
+
 });
+
 
 module.exports = router;
