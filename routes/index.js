cc974f29026108ecacd39af3bff028f9f6565939
sisisin <azsisisin@gmail.com>
2015-05-09 14:00:59 -0400

    add work 'view url list' and 'add url'

--- routes/index.js
+++ routes/index.js
@@ -10,49 +10,46 @@ router.get('/', function(req, res, next) {
     host: '192.168.1.15',
     user: 'appUser',
     password: 'kszk3081abk',
+    db:'urlList'
   });
   
-  if (false && !req.param('url') && !req.param('title')) {
-    c.query('INSERT INTO urlList.urlListTitles (url,title)\n' +
-      'VALUES ('+req.param('url')+ ','+req.param('title')+');');
+  var sql = '';
+
+  
+  if (!!req.query.url && !!req.query.title) {
+
+    sql = 'INSERT INTO urlListTitles (url,title) VALUES ( :url, :title); ';      
+    sql += 'INSERT INTO urlListStatus (url) VALUES ( :url );';   
+     c.query(sql,{
+       url:req.query.url,
+       title:req.query.title
+       })
+    .on('error',function (err) {
+      console.log(err);
+    });
   }
   
-  var sql = 'use urlList;\n';
-  c.query(sql);
-  
+
+  var obj = {};
+  obj.title = 'Express';
+  obj.list = [];
   sql = 'select * from urlListTitles inner join urlListStatus on urlListTitles.url = urlListStatus.url;';
   c.query(sql)
     .on('result',function (queryRes) {
       queryRes.on('row',function (row) {
 
-        var obj = {};
-        obj.title = 'Express';
-      
-        obj.list = [{
-          title:'google',
-          url:'http://google.co.jp',
-          sts:0
-        },
-        {
-          title:'yahoo',
-          url:'http://yahoo.co.jp',
-          sts:1
-        },
-        {
+        obj.list.push({
           title:row.title,
           url:row.url,
           sts:row.sts
-        }
-        ];
-        
-        res.render('index',obj);              
+        });
       })
       .on('end',function (info) {
-
+        res.render('index',obj);              
       });
     });
   
-  
+  c.end();
 
 });
 
