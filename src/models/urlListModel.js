2d27f6e2b0c16cb9c632d933f2d069682050e779
sisisin <azsisisin@gmail.com>
2015-09-25 13:45:03 +0900

    Promise化でミスってたとこ修正

--- src/models/urlListModel.js
+++ src/models/urlListModel.js
@@ -35,26 +35,31 @@ export function updateUrlList (c, url, title) {
 };
 
 export function deleteUrlList (c, argUrl) {
-	return new Promise((resolve, reject) => {
-		let arrUrl,query;
-		arrUrl = _.isArray(argUrl) ? argUrl : [argUrl];
-			
-		query = 'UPDATE urlListStatus SET sts = 9 ';
-		_.forEach(arrUrl, (val, i) => i === 0 ? query += `url = ${val} ` : query += `or url = ${val} `);
-		return getExecuteQueryPromise(c, query);
+	let arrUrl,query;
+	arrUrl = _.isArray(argUrl) ? argUrl : [argUrl];
+		
+	query = 'UPDATE urlListStatus SET sts = 9';
+	query += ' WHERE';
+	_.forEach(arrUrl, (val, i) => {
+		if (i !== 0) query += ` or`;
+		query += ` url = '${val}'`;
 	});
+	query += ';';
+
+	return getExecuteQueryPromise(c, query);
 };
 
 function getExecuteQueryPromise(client, query, queryArg = undefined) {
 	return new Promise((resolve, reject) => {
-		let result = [];
+		let result;
 		client.query(query, queryArg)
 			.on('result', (res) => {
+				let rows = [];
 				res
 				.on('error', reject)
-				.on('row', (row) => { if (row) result.push(row); })
-				.on('end', (info) => { resolve(result || info); });
-			})
+				.on('row', (row) => { if (row) rows.push(row); })
+				.on('end', (info) => { result = rows.length === 0 ? info : rows; }); })
 			.on('error', reject)
+			.on('end', () => { resolve(result); });
 	});	
 }
