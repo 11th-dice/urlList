e756282510b64c13386906dc486169de91cfd2e3
sisisin <azsisisin@gmail.com>
2015-11-23 15:32:30 +0900

    もういっちょ

--- src/public/js/bookmarklet.js
+++ src/public/js/bookmarklet.js
@@ -1,7 +1,7 @@
 (function () {
 	function setUrl() {
 		var xhr = new XMLHttpRequest();
-    var url = encodeURIComponent('http://192.168.1.3:3000/?url=' + location.href + '&title=' + document.title);
+    var url = 'http://192.168.1.3:3000/?url=' + encodeURIComponent(location.href) + '&title=' + encodeURIComponent(document.title);
 		xhr.open('POST', url, true);
 		xhr.send(null);
 	}
