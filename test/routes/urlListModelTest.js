6b6ebc6de4b97ee1208872c6ea33d66ae0977d8d
sisisin <azsisisin@gmail.com>
2015-12-07 23:59:03 +0900

    add route tests (pending

---
+++ test/routes/urlListModelTest.js
@@ -0,0 +1,14 @@
+import {indexRoutes} from 'chai';
+
+describe('route get / index 200', function () {
+	it('modelに正常に値が入っている');
+});
+
+describe('route post / index 200', function () {
+	it('DBがアップデートされている');
+});
+
+describe('route get /delete index 200', function () {
+	it('削除されている');
+});
+
